82f742352d318a7ed03e189f3498d5757f5d775e
Tiago Pierezan Camargo <tcamargo@gmail.com>
2008-04-24 22:16:47 +0200

    Solution of problem 113.

---
+++ p113.c
@@ -0,0 +1,29 @@
+/* Problema 113 do online-judge
+ * Power of Cryptography
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <math.h>
+
+
+int main (int argc, char const* argv[]) {
+
+	double n, p, k;
+
+
+#ifndef ONLINE_JUDGE
+	close(0); open("/home/scm/online-judge/p113.in", O_RDONLY);
+//	close(1); open("/home/scm/online-judge/p113.out", O_WRONLY | O_CREAT, 0600);
+#endif
+
+	while (scanf("%lf\n", &n) == 1) {
+		scanf("%lf\n", &p);
+		k = exp(log(p)/n);
+		printf("%.0f\n", k);
+
+	}
+	return 0;
+}
